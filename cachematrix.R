4bc64ef25a78d683641a81f3f2de118f66ade39f
Norman Byer <normbyer@gmail.com>
2015-12-22 10:28:53 -0500

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are used for when one needs to invert a matrix
+## and get the result multiple times without recalculating.
+## "makeCacheMatrix" sets up the matrix and needed functions
+## "cacheSolve" returns the inverted matrix
 
-## Write a short comment describing this function
+## returns a list that can be used with the "cacheSolve" function
+## a matrix can be passed or set later by passing it to the "Set" 
+## function returned in the returned list. The value of the matrix
+## can be  retrieved by the "Get" function returned in the list.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  Matrix <- x
+  Inverse <- NA
+  Set <- function(value){
+    Matrix <<- value
+    Inverse <<- NA
+  }
+  Get <- function(){
+    Matrix
+  }
+  SetInv <- function(value){
+    Inverse <<- value
+  }
+  GetInv <- function() {
+    Inverse
+  }
+  return(list("Set" = Set, "Get" = Get, "SetInv" = SetInv, "GetInv" = GetInv))
 }
 
 
-## Write a short comment describing this function
+
+## runs the solve function on a list generated from the "makeCacheMatrix"
+## function and stores the result so future calls to this function will
+## not have to run solve unless the data has changed.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if(length(x) > 1 || is.na(x$GetInv())){ ##the length condition is to supress
+                                          ##a warning that is.na generates for
+                                          ##things with alength greater than 1
+    x$SetInv(solve(x$Get(),...))
+  }
+  return(x$GetInv())
 }
